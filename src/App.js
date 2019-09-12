8a60d1322f90c597c19afa91431f0d3f1477224a
Johnny Rocksalt <9876543+johnpgeipel@users.noreply.github.com>
2019-09-12 18:01:05 -0500

    doing a master pull

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import './App.css';
-import "./styles.css";
+import './style.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+        <h1>SPEDtrack</h1>
+          <a href="/reg-ed" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Reg Ed</a>
+          <a href="/sped" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">SPED</a>
+          <a href="/newUser" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">New User</a>
+      
+     
     </div>
   );
 }
---
+++ src/pages/RegED.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+return (
+    <div>
+        <h1>Reg ED Page</h1>
+    </div>
+)
