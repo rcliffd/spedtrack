3bb593aeda2125866ec58e79ebaaa5bc0abbef9e
cclivick <46388110+cclivick@users.noreply.github.com>
2019-09-13 18:00:58 -0500

    Fixed database communication issue

--- controllers/schoolsController.js
+++ controllers/schoolsController.js
@@ -1,4 +1,4 @@
-const db = require("../models");
+const db = require("../models/school");
 
 module.exports = {
     findAll: function(req, res) {
--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -1,4 +1,4 @@
-const db = require("../models");
+const db = require("../models/student");
 
 module.exports = {
     find: function(req, res) {
