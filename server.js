32ab1865b0edfe5330c4f46a9a4fec54e04640f6
Ash <Ashleigh.lund@gmail.com>
2019-09-14 14:36:18 -0500

    concurrent

--- server.js
+++ server.js
@@ -5,25 +5,18 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-import passport from 'passport';
 
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(passport.initialize());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
 }
 // Routes
 app.use(routes);
 
-require('./routes/loginUser')(app);
-require('./routes/registerUser')(app);
-require('./routes/finduser')(app);
-require('./routes/deleteUser')(app);
-require('./routes/updateuser')(app);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/spedtrack");
