30e7bab4d3b8bc326dcc83d93c84e935f016b3c1
Johnny Rocksalt <9876543+johnpgeipel@users.noreply.github.com>
2019-09-19 19:14:11 -0500

    Adjucted Hours/Blocks

--- src/components/Hours/Hours.js
+++ src/components/Hours/Hours.js
@@ -5,14 +5,18 @@ function Hours(props) {
        
             
             <div className="col-md-6">
-            <h4>Hours</h4>
+            <h4>Hours/Block</h4>
                                 <select className="form-control" name="hours" onChange={props.handleInputChange}>
                                 {/**put onchagne here */}
-                                <option value="1">placeholder</option>
-                                <option value="2">placeholder</option>
-                                <option value="3">placeholder</option>
-                                <option value="4">placeholder</option>
-                                <option value="5">placeholder</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
                                 
                                 </select>
                             </div>
