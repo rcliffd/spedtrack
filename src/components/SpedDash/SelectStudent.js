f88c935f7236a4e8adb345f503f50d6cfa3b8071
Johnny Rocksalt <9876543+johnpgeipel@users.noreply.github.com>
2019-09-26 18:43:58 -0500

    commit

--- src/components/SpedDash/SelectStudent.js
+++ src/components/SpedDash/SelectStudent.js
@@ -11,13 +11,6 @@ export function StudentOption(props) {
 
 export function StudentSelect(props) {
     return (
-<<<<<<< HEAD
-        <div>
-        <h4>Select Student</h4>
-        <select className="form-control">{children}</select>
-        </div>
-=======
         <select className="studentSelect" onChange={props.getStudentById}>{props.children}</select>
->>>>>>> 37b0469c76f5dfdf660b0482f01f7209ece61638
     )
 }
