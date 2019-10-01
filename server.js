5797a10bfa356a5535691b8a6e693af3c7d929a0
cclivick <46388110+cclivick@users.noreply.github.com>
2019-10-01 14:52:30 -0500

    Returning to working version (local) of server and package.json

--- server.js
+++ server.js
@@ -4,20 +4,19 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/spedtrack";
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static("client/build"));
 }
 // Routes
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/spedtrack");
 
 // Start the API server
 app.listen(PORT, function() {
